e54abcf016eb847867dd64b2e88b879f5aa566ff
Yunhe (John) Wang <yhjw88@gmail.com>
2018-05-27 23:21:38 +0000

    Fixed loading bugs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
@@ -17,7 +18,7 @@ def parse_args():
     parser.add_argument('--output', type=str, default='saved_models/exp0')
     parser.add_argument('--batch_size', type=int, default=512)
     parser.add_argument('--seed', type=int, default=1111, help='random seed')
-    parser.add_argument('--eval_only', type=str, default=False)
+    parser.add_argument('--eval_only', type=bool, default=False)
     args = parser.parse_args()
     return args
 
@@ -31,26 +32,31 @@ if __name__ == '__main__':
 
     dictionary = Dictionary.load_from_file('data/dictionary.pkl')
     if not args.eval_only:
+        print "Fetching train data"
         train_dset = VQAFeatureDataset('train', dictionary)
-    eval_dset = VQAFeatureDataset('val', dictionary)
-    batch_size = args.batch_size
+    print "Fetching eval data"
+    eval_dset = VQAFeatureDataset('valSample', dictionary)
 
     # Fetch model.
     constructor = 'build_%s' % args.model
-    model = getattr(base_model, constructor)(train_dset, args.num_hid).cuda()
+    if not args.eval_only:
+        model = getattr(base_model, constructor)(train_dset, args.num_hid).cuda()
+    else:
+        model = getattr(base_model, constructor)(eval_dset, args.num_hid).cuda()
     model.w_emb.init_embedding('data/glove6b_init_300d.npy')
     model = nn.DataParallel(model).cuda()
     if args.load_path:
-        print "Loading model from {}".format(args.load_path)
-        checkpoint = torch.load(args.load_path)
-        model.load_state_dict(checkpoint["state_dict"])
+        load_path = os.path.join(args.load_path, 'model.pth')
+        print "Loading model from {}".format(load_path)
+        model.load_state_dict(torch.load(load_path))
 
     # Train/eval.
-    eval_loader =  DataLoader(eval_dset, batch_size, shuffle=True)
+    eval_loader =  DataLoader(eval_dset, args.batch_size, shuffle=True)
     if not args.eval_only:
-        train_loader = DataLoader(train_dset, batch_size, shuffle=True)
+        train_loader = DataLoader(train_dset, args.batch_size, shuffle=True)
         train.train(model, train_loader, eval_loader, args.epochs, args.output)
     else:
+        print "Evaluating..."
         model.train(False)
         eval_score, bound = train.evaluate(model, eval_loader)
         print "eval score: %.2f (%.2f)" % (100 * eval_score, 100 * bound)
