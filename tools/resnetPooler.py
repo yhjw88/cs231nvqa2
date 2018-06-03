92bc6815de118af318167d025a6166ff5ec066e6
Yunhe (John) Wang <yhjw88@gmail.com>
2018-06-03 21:29:34 +0000

    added truncater to pooler

--- tools/resnetPooler.py
+++ tools/resnetPooler.py
@@ -1,4 +1,5 @@
 import h5py
+import json
 import torch
 
 def poolFeatures(inFilename, outFilename):
@@ -21,7 +22,45 @@ def poolFeatures(inFilename, outFilename):
                 print "Processed {}".format(i+1)
     print "Done"
 
+def truncateFile(inFilename, inIdxFilename, outFilename, outIdxFilename, imageDictFilename):
+    imageDict = json.load(open(imageDictFilename))
+    imageIdToIdx = cPickle.load(open(inIdxFilename))
+
+    idxToImageId = {}
+    for imageId in imageDict:
+        imageId = int(imageId)
+        idxToImageId[imageIdToIdx[imageId]] = imageId
+    print "Extracting {} images".format(len(idxToImageId))
+
+
+    with h5py.File(inFilename, "r") as inFile, \
+        h5py.File(outFilename, "w") as outFile:
+
+        inFeatures = inFile["image_features"]
+        outFeatures = outFile.create_dataset(
+            "image_features",
+            (len(imageDict), 49, 2048),
+            "f")
+
+        counter = 0
+        indices = {}
+        for i, inFeature in enumerate(inFeatures):
+            if i in idxToImageId:
+                outFeatures[counter, :, :] = inFeature
+                indices[idxToImageId[i]] = counter
+                counter += 1
+
+        print "Extracted {} images".format(len(indices))
+        cPickle.dump(indices, open(outIdxFilename, "wb"))
+
 if __name__ == "__main__":
-    poolFeatures(
-        "data/resnet/train49.hdf5",
-        "data/resnet/train9.hdf5")
+    # poolFeatures(
+    #     "data/resnet/train49.hdf5",
+    #     "data/resnet/train9.hdf5")
+
+    truncateFile(
+        "data/resnet/val49.hdf5",
+        "data/resnet/val49Idx.pkl",
+        "data/resnet/valSample49.hdf5",
+        "data/resnet/valSample49Idx.pkl",
+        "data/valSample2014imgs.json")
