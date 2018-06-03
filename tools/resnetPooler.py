1f6c6ccbf39cfc50b3c7eabdc196c51a6a2f1528
Yunhe (John) Wang <yhjw88@gmail.com>
2018-06-03 10:18:24 +0000

    Fixed bugs in resnetPooler

--- tools/resnetPooler.py
+++ tools/resnetPooler.py
@@ -1,7 +1,7 @@
 import h5py
 import torch
 
-def poolFeatures(args, inFilename, outFilename):
+def poolFeatures(inFilename, outFilename):
     with h5py.File(inFilename, "r") as inFile, \
         h5py.File(outFilename, "w") as outFile:
 
@@ -13,8 +13,11 @@ def poolFeatures(args, inFilename, outFilename):
 
         pool = torch.nn.AvgPool2d(3, stride=2)
         for i, inFeature in enumerate(inFeatures):
+            # We reshaped (2048, 7, 7) to (49, 2048) by accident.
+            # Thus, we must correct it here.
+            inFeature = inFeature.reshape((2048, 7, 7))
             outFeature = pool(torch.from_numpy(inFeature))
-            outFeatures[i, :, :] = outFeature.numpy()
+            outFeatures[i, :, :] = outFeature.numpy().transpose([1, 2, 0]).reshape((9, 2048))
 
             if (i+1)%500 == 0:
                 print "Processed {}".format(i+1)
