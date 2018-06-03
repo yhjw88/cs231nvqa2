167d648e55bf84b81108597cfa158859e855b310
Yunhe (John) Wang <yhjw88@gmail.com>
2018-06-02 18:01:24 -0700

    Preparation for new train mode

--- main.py
+++ main.py
@@ -18,11 +18,56 @@ def parse_args():
     parser.add_argument('--output', type=str, default='saved_models/exp0')
     parser.add_argument('--batch_size', type=int, default=512)
     parser.add_argument('--seed', type=int, default=1111, help='random seed')
-    parser.add_argument('--eval_only', type=bool, default=False)
+    parser.add_argument('--mode', type=str, default='normal')
     parser.add_argument('--evalset_name',type=str,default='val')
     args = parser.parse_args()
     return args
 
+def trainNormal(args):
+    # Fetch data.
+    dictionary = Dictionary.load_from_file('data/dictionary.pkl')
+    print "Fetching train data"
+    train_dset = VQAFeatureDataset('train', dictionary)
+    print "Fetching eval data"
+    eval_dset = VQAFeatureDataset('val', args.evalset_name, dictionary)
+
+    # Fetch model.
+    constructor = 'build_%s' % args.model
+    model = getattr(base_model, constructor)(train_dset, args.num_hid).cuda()
+    model.w_emb.init_embedding('data/glove6b_init_300d.npy')
+    model = nn.DataParallel(model).cuda()
+    if args.load_path:
+        load_path = os.path.join(args.load_path, 'model.pth')
+        print "Loading model from {}".format(load_path)
+        model.load_state_dict(torch.load(load_path))
+
+    # Train.
+    train_loader = DataLoader(train_dset, args.batch_size, shuffle=True)
+    eval_loader =  DataLoader(eval_dset, args.batch_size, shuffle=True)
+    train.train(model, train_loader, eval_loader, args.epochs, args.output)
+
+def evalNormal(args):
+    # Fetch data.
+    dictionary = Dictionary.load_from_file('data/dictionary.pkl')
+    print "Fetching eval data"
+    eval_dset = VQAFeatureDataset('val', args.evalset_name, dictionary)
+
+    # Fetch model.
+    constructor = 'build_%s' % args.model
+    model = getattr(base_model, constructor)(eval_dset, args.num_hid).cuda()
+    model.w_emb.init_embedding('data/glove6b_init_300d.npy')
+    model = nn.DataParallel(model).cuda()
+    if args.load_path:
+        load_path = os.path.join(args.load_path, 'model.pth')
+        print "Loading model from {}".format(load_path)
+        model.load_state_dict(torch.load(load_path))
+
+    # Evaluate
+    eval_loader =  DataLoader(eval_dset, args.batch_size, shuffle=True)
+    print "Evaluating..."
+    model.train(False)
+    eval_score, bound = train.evaluate(model, eval_loader)
+    print "eval score: %.2f (%.2f)" % (100 * eval_score, 100 * bound)
 
 if __name__ == '__main__':
     args = parse_args()
@@ -31,33 +76,9 @@ if __name__ == '__main__':
     torch.cuda.manual_seed(args.seed)
     torch.backends.cudnn.benchmark = True
 
-    dictionary = Dictionary.load_from_file('data/dictionary.pkl')
-    if not args.eval_only:
-        print "Fetching train data"
-        train_dset = VQAFeatureDataset('train', dictionary)
-    print "Fetching eval data"
-    eval_dset = VQAFeatureDataset('val', args.evalset_name, dictionary)
-
-    # Fetch model.
-    constructor = 'build_%s' % args.model
-    if not args.eval_only:
-        model = getattr(base_model, constructor)(train_dset, args.num_hid).cuda()
+    if args.mode == "train":
+        trainNormal(args)
+    elif args.mode == "eval":
+        evalNormal(args)
     else:
-        model = getattr(base_model, constructor)(eval_dset, args.num_hid).cuda()
-    model.w_emb.init_embedding('data/glove6b_init_300d.npy')
-    model = nn.DataParallel(model).cuda()
-    if args.load_path:
-        load_path = os.path.join(args.load_path, 'model.pth')
-        print "Loading model from {}".format(load_path)
-        model.load_state_dict(torch.load(load_path))
-
-    # Train/eval.
-    eval_loader =  DataLoader(eval_dset, args.batch_size, shuffle=True)
-    if not args.eval_only:
-        train_loader = DataLoader(train_dset, args.batch_size, shuffle=True)
-        train.train(model, train_loader, eval_loader, args.epochs, args.output)
-    else:
-        print "Evaluating..."
-        model.train(False)
-        eval_score, bound = train.evaluate(model, eval_loader)
-        print "eval score: %.2f (%.2f)" % (100 * eval_score, 100 * bound)
+        print "Mode not supported: {}".format(args.mode)
